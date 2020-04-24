6c9165d5bf7b48b3f507d2ba34c860f0b7b818a4
Luiza de Avila Tavares <luiza.tavares@ntconsult.com.br>
2020-04-23 23:08:02 -0300

    First version

--- lib/database.dart
+++ lib/database.dart
@@ -1,10 +1,8 @@
 import 'dart:async';
 import 'dart:io';
-
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
-
 import 'client_model.dart';
 
 class DBProvider {
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:math' as math;
-
 import 'client_model.dart';
 import 'database.dart';
 
@@ -9,76 +7,128 @@ void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
     ));
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
+class MyApp extends StatelessWidget {
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        title: 'Clientes',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MyHomePage(title: 'Clientes'),
+      );
+    }
 }
 
-class _MyAppState extends State<MyApp> {
-  // data for testing
-  List<Cliente> testClients = [
-    Cliente(nome: "Dick", sobrenome: "Vigarista", marcado: false),
-    Cliente(nome: "Penélope", sobrenome: "Charmosa", marcado: true),
-    Cliente(nome: "Medinho", sobrenome: "Beltrano", marcado: false),
-    Cliente(nome: "Muttley", sobrenome: "Siclano", marcado: false),
-  ];
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  
+  List<dynamic> _clientes = [];
+
+  List<Widget> get _widgets =>
+    _clientes.map((cliente) => clientesWidget(cliente)).toList();
+
+    Widget clientesWidget(Cliente cliente) {
+    return Dismissible(
+      child: Padding(
+        padding: EdgeInsets.all(16),
+        child: FlatButton(
+          child: Row(
+            children: <Widget>[
+              Text(cliente.nome),
+              Icon(cliente.marcado == true
+                  ? Icons.radio_button_checked
+                  : Icons.radio_button_unchecked),
+            ],
+          ), onPressed: () {},
+        ),
+      ), key: null,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("CDMA22 - Clientes"),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.delete),
-            onPressed: () {
-              DBProvider.db.deleteAll();
-              setState(() {});
-            },
-          )
-        ],
+        title: Text(widget.title),
       ),
-      body: FutureBuilder<List<Cliente>>(
-        future: DBProvider.db.getAllClientes(),
-        builder: (BuildContext context, AsyncSnapshot<List<Cliente>> snapshot) {
-          if (snapshot.hasData) {
-            return ListView.builder(
-              itemCount: snapshot.data.length,
-              itemBuilder: (BuildContext context, int index) {
-                Cliente item = snapshot.data[index];
-                return Dismissible(
-                  key: UniqueKey(),
-                  background: Container(color: Colors.red),
-                  onDismissed: (direction) {
-                    DBProvider.db.deleteCliente(item.id);
-                  },
-                  child: ListTile(
-                    title: Text(item.nome + " " + item.sobrenome),
-                    leading: Text(item.id.toString()),
-                    trailing: Checkbox(
-                      onChanged: (bool value) {
-                        DBProvider.db.blockOrUnblock(item);
-                        setState(() {});
-                      },
-                      value: item.marcado,
-                    ),
-                  ),
-                );
-              },
-            );
-          } else {
-            return Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
+      body: Center(
+          child: ListView(
+        children: _widgets,
+      )),
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () async {
-          Cliente rnd = testClients[math.Random().nextInt(testClients.length)];
-          await DBProvider.db.newCliente(rnd);
-          setState(() {});
+        child: Icon(Icons.add_circle_outline),
+        onPressed: () {
+          _addCliente(context);
         },
       ),
     );
+  }
+
+  String _clienteNome;
+  String _clienteSobrenome;
+
+  void _addCliente(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Adicionar Tarefa"),
+          actions: <Widget>[
+            FlatButton(
+              onPressed: () => _salvarCliente(),
+              child: Text("Incluir"),
+            ),
+            FlatButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text("Cancelar"),
+            ),
+          ],
+          content: Form(
+            child: Column(children: <Widget>[
+              TextField(
+                autofocus: true,
+                decoration: InputDecoration(
+                  labelText: "Nome:",
+                  hintText: "Insira o primeiro nome",
+                ),
+                onChanged: (valor) {
+                  _clienteNome = valor;
+                },
+              ),
+              TextField(
+                autofocus: true,
+                decoration: InputDecoration(
+                  labelText: "Sobrenome:",
+                  hintText: "Insira o Sobrenome",
+                ),
+                onChanged: (valor) {
+                  _clienteSobrenome = valor;
+                },
+              )
+            ],),
+          ) 
+        );
+      },
+    );
+  }
+  _salvarCliente() async {
+    Cliente cliente = Cliente(
+      nome: _clienteNome,
+      sobrenome: _clienteSobrenome,
+      marcado: true,
+    );
+
+    await DBProvider.db.newCliente(cliente);
+    setState(() => _clienteNome = '');
+    setState(() => _clienteSobrenome = '');
+    Navigator.of(context).pop();
   }
 }
